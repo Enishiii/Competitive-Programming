3d8a92b16ca0048cac2f1cd1e4b5905bd6ef30a4
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-02-18 10:54:57 +0900

    add spaces

--- ABC135/c.cpp
+++ ABC135/c.cpp
@@ -11,14 +11,13 @@ int main() {
 
     long long ans = 0;
     for (int i = 0; i < n; ++i) {
+        
         long long lft = min(a[i], b[i]);
-
         ans += lft;
         a[i] -= lft;
         b[i] -= lft;
 
         long long rht = min(a[i+1], b[i]);
-
         ans += rht;
         a[i+1] -= rht;
         b[i] -= rht;
