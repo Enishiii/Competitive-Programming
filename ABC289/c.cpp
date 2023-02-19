5d4537587e5dc64023fe9b0eb642012990b5fc54
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-02-19 12:36:19 +0900

    ABC289 C問題
    bit全探索を使って、選び方のパターンを全て調べる

---
+++ ABC289/c.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> a(m);
+    for (int i = 0; i < m; ++i) {
+        int c;
+        cin >> c;
+        a[i].resize(c);
+        for (auto& x : a[i]) cin >> x;
+    }
+
+    int ans = 0;
+    for (int b = 0; b < (1 << m); ++b) {
+        set<int> s;
+        for (int i = 0; i < m; ++i) {
+            if ((b >> i) & 1) {
+                for (auto& x : a[i]) s.insert(x);
+            }
+        }
+        ans += (int)s.size() == n;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
