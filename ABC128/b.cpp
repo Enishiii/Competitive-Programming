e7392f422a876a05f3706e27aecb66fe8b61facb
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-03-02 10:24:12 +0900

    ラムダ式解法

--- ABC128/b.cpp
+++ ABC128/b.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-    pair<pair<string, int>, int> p[110];
+    vector<string> s(n);
+    vector<int> p(n);
+    for (int i = 0; i < n; ++i) cin >> s[i] >> p[i];
+
+    vector<int> ans;
+    for (int i = 0; i < n; ++i) ans[i] = i;
+
+    sort(ans.begin(), ans.end(), [&](int a, int b){
+        if (s[a] != s[b]) return s[a] < s[b];
+        return p[a] > p[b];
+    });
+
     for (int i = 0; i < n; ++i) {
-        string s;
-        int q;
-        cin >> s >> q;
-        p[i] = make_pair(make_pair(s, -q), i);
-    }
-    sort(p, p + n);
-    for (int i = 0; i < n; ++i) {
-        cout << p[i].second+1 << endl;
+        cout << ans[i]+1 << endl;
     }
 
     return 0;
