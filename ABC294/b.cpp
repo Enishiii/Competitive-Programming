ce32cdee9670e469181d0ed745acc28a0f0a2ba2
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-03-20 11:19:15 +0900

    実装をシンプルにリファクタリング

--- ABC294/b.cpp
+++ ABC294/b.cpp
@@ -1,26 +1,18 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <set>
-#include <map>
 using namespace std;
 
 int main() {
     int h, w;
     cin >> h >> w;
-    vector a(h, vector<int>(w));
+    
     for (int i = 0; i < h; ++i) {
+        string s(w, '.');
         for (int j = 0; j < w; ++j) {
-            cin >> a[i][j];
+            int x;
+            cin >> x;
+            if (x != 0) s[j] = 'A' + x - 1;
         }
-    }
-    string alpha = ".ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    for (int i = 0; i < h; ++i) {
-        for (int j = 0; j < w; ++j) {
-            cout << alpha[a[i][j]];
-        }
-        cout << endl;
+        cout << s << '\n';
     }
 
     return 0;
