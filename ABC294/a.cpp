b711c5191d7ed03707763ff2e65b15d3a8c3959d
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-03-20 11:14:08 +0900

    配列で持つ必要ないので、削除

--- ABC294/a.cpp
+++ ABC294/a.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <set>
-#include <map>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-    vector<int> a(n);
     for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-        if (a[i] % 2 == 0) cout << a[i] << ' ';
+        int a;
+        cin >> a;
+        if (a % 2 == 0) cout << a << ' ';
     }
     cout << endl;
     return 0;
