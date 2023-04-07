b92fe10311ec5cf81c51b3abc542c11c96c82bb4
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-04-07 14:00:04 +0900

    より洗練された解法

--- ABC/ABC096/b.cpp
+++ ABC/ABC096/b.cpp
@@ -5,15 +5,7 @@ int main() {
     int a, b, c, k;
     cin >> a >> b >> c >> k;
 
-    if (a >= b && a >= c) {
-        for (int i = 0; i < k; ++i) a *= 2;
-    } else if (b >= a && b >= c) {
-        for (int i = 0; i < k; ++i) b *= 2;
-    } else if (c >= a && c >= b) {
-        for (int i = 0; i < k; ++i) c *= 2;
-    }
-
-    cout << a+b+c << endl;
+    cout << a + b + c + max(a, max(b, c)) * ((1 << k) - 1) << endl;
 
     return 0;
 }
