4eac06e252d8b26bd23be2eb3942c3f86543107d
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-04-09 23:25:50 +0900

    自力AC

---
+++ ABC/297/b.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 入力を受け取る
+    string s;
+    cin >> s;
+
+    // 各文字のイテレータを格納する変数を用意して、代入していく
+    int x = -1, y = -1, a = -1, b = -1, c = -1;
+    for (int i = 0; i < 8; ++i) {
+        if (s[i] == 'B') {
+            if (x == -1) x = i;
+            if (x != -1) y = i;
+        }
+
+        if (s[i] == 'R') {
+            if (a == -1) a = i;
+            if (a != -1) c = i;
+        }
+        if (s[i] == 'K') b = i;
+    }
+
+    // 問題文の条件を満たすか判定g
+    if ((a < b && b < c) && (x % 2 != y % 2)) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
