8a123e951bcb26f8c6c6be69830dd997e2308a7b
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-04-16 14:48:34 +0900

    自力AC

---
+++ ABC/298/a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+using namespace std;
+
+int main()
+{
+    int n;
+    string s;
+    cin >> n >> s;
+
+    bool ok = false;
+    for (int i = 0; i < n; ++i) {
+        if (s[i] == 'x') {
+            cout << "No" << endl;
+            return 0;
+        }
+        if (s[i] == 'o') ok = true;
+    }
+    if (ok) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
