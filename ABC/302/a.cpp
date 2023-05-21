59dde144b9cb4114b06dfefbae7978ae83b71dd7
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-05-21 09:18:38 +0900

    問題文と正しいコードに修正

--- ABC/302/a.cpp
+++ ABC/302/a.cpp
@@ -1,11 +1,24 @@
+/* 問題文
+体力がA の敵がいます。あなたは、1 回攻撃をすることで敵の体力を
+B 減らすことが出来ます。
+敵の体力を0 以下にするためには、最小で何回攻撃をする必要があるでしょうか？
+
+制約
+1≤A,B≤10^18
+A,B は整数である。 */
+
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
-    int h, w;
-    cin >> h >> w;
-    vector<string> s(h);
+    long long a, b;
+    cin >> a >> b;
+
+    long long ans = a / b;
+    if (a % b == 0)
+        cout << ans << endl;
+    else
+        cout << ans + 1 << endl;
 
     return 0;
 }
