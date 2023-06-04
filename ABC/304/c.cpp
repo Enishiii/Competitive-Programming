c56f68474b6d85719065349e037296303bccea1a
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-06-04 10:25:33 +0900

    コメントを追加

--- ABC/304/c.cpp
+++ ABC/304/c.cpp
@@ -18,11 +18,14 @@ i != j のとき(Xi,Yi) != (Xj,Yj)
 #include <cmath>
 using namespace std;
 
+// Union-Find木を実装するための構造体
 struct UnionFind {
+    // 各ノードの親を保持する配列。初期値は-1(自身がルートを意味する)
     vector<int> par;
 
     UnionFind(int n) : par(n, -1) {}
 
+    // ノードxのルートを取得
     int root(int x) {
         if (par[x] < 0)
             return x;
@@ -30,6 +33,7 @@ struct UnionFind {
             return par[x] = root(par[x]);
     }
 
+    // ノードxとyを結合する
     bool unite(int x, int y) {
         x = root(x);
         y = root(y);
@@ -42,14 +46,18 @@ struct UnionFind {
         return true;
     }
 
+    // ノードxとyが同じ集合に属しているかどうかを判断
     bool same(int x, int y) {
         return root(x) == root(y);
     }
 };
 
 int main() {
+    // 人数nと感染距離dを取得
     int n, d;
     cin >> n >> d;
+
+    // 各人の位置を保持するベクトル
     vector<int> x(n), y(n);
     for (int i = 0; i < n; ++i)
         cin >> x[i] >> y[i];
@@ -57,12 +65,14 @@ int main() {
     UnionFind uf(n);
     for (int i = 0; i < n; ++i) {
         for (int j = i + 1; j < n; ++j) {
+            // ユークリッド距離が感染距離以下の場合、2人を同じグループに結合
             if (sqrt(pow(x[i] - x[j], 2) + pow(y[i] - y[j], 2)) <= d) {
                 uf.unite(i, j);
             }
         }
     }
 
+    // 各人がウイルスに感染しているか（人1と同じグループにいるか)を表示
     for (int i = 0; i < n; ++i) {
         if (uf.same(0, i))
             cout << "Yes" << endl;
