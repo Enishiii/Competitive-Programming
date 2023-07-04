b87a4b8a1ce666c184baa92bb3a75bd7cfeb9073
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-07-04 15:33:17 +0900

    問題文とコメントを追加

--- ABC/308/a.cpp
+++ ABC/308/a.cpp
@@ -1,21 +1,29 @@
+/* 問題文
+8 個の整数S1,S2,…,S8 が与えられます。 以下の3 つの条件が全て満たされるならば Yes を、そうでないならば No を出力してください。
+    ・数列(S1,S2,…,S8) は広義単調増加である。すなわち、S1≤S2≤⋯≤S8 である。
+    ・S1,S2,…,S8 は全て100 以上675 以下である。
+    ・S1,S2,…,S8 は全て25 の倍数である。
+
+制約
+0≤Si≤1000
+入力は全て整数 */
+
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
-int main()
-{
+// 問題文の通りに素直に実装する
+
+int main() {
     int s[8];
     bool ok = true;
-    for (int i = 0; i < 8; ++i)
-    {
+
+    for (int i = 0; i < 8; ++i) {
         cin >> s[i];
         if (s[i] < 100 || s[i] > 675)
             ok = false;
         if (s[i] % 25 != 0)
             ok = false;
-        if (i > 0)
-        {
+        if (i > 0) {
             if (s[i] < s[i - 1])
                 ok = false;
         }
