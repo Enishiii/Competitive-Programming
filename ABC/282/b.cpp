625d2344012d4f9d5489c2e7423e0006f713d244
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-07-13 21:59:21 +0900

    問題文追加＆リファクタリング

--- ABC/282/b.cpp
+++ ABC/282/b.cpp
@@ -1,8 +1,18 @@
+/* 問題文
+1 からN までの番号がついたN 人の参加者が、1 からM までの番号がついた
+M 問からなるコンテストに参加します。
+1 以上N 以下の整数i 、1 以上M 以下の整数j について、Si のj 番目の文字が o のとき参加者i は問題j を解くことが可能で、Si のj 番目の文字が x のとき参加者i は問題j を解くことが不可能です。
+このコンテストは、二人の参加者でペアを組んで参加します。二人が協力することでM 問全てを解くことが可能であるようなペアの個数を答えてください。
+より厳密には、1≤x<y≤N を満たす整数の組(x,y) であって、1 以上M 以下の任意の整数j について、参加者x か参加者y の少なくとも一方は問題j を解くことが可能であるという条件を満たすものの個数を答えてください。
+
+制約
+N は2 以上30 以下の整数
+M は1 以上30 以下の整数
+Si は o, x からなる長さM の文字列 */
+
 #include <iostream>
 #include <vector>
 using namespace std;
-
-using ll = long long;
 
 int main() {
     int n, m;
@@ -12,9 +22,10 @@ int main() {
     for (int i = 0; i < n; ++i) cin >> s[i];
 
     int ans = 0;
+
+    // 制約が小さいので、二重ループで全探索して条件を満たすかを判定する
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (i == j) continue;
+        for (int j = i+1; j < n; ++j) {
             bool ok = true;
             for (int k = 0; k < m; ++k) {
                 if (s[i][k] == 'x' && s[j][k] == 'x') ok = false;
@@ -22,7 +33,7 @@ int main() {
             if (ok) ans++;
         }
     }
-    cout << ans / 2 << endl;
+    cout << ans << endl;
 
     return 0;
 }
