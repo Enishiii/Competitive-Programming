cd7f4db2ac2261ac8b023ee1fc65d94ed1c7e442
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-07-22 22:41:50 +0900

    自力AC

---
+++ ABC/311/a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    bool a = false;
+    bool b = false;
+    bool c = false;
+    for (int i = 0; i < n; ++i) {
+        if (s[i] == 'A') a = true;
+        else if (s[i] == 'B') b = true;
+        else if (s[i] == 'C') c = true;
+
+        if (a && b && c) {
+            cout << i + 1 << endl;
+            return 0;
+        }
+    }
+
+    return 0;
+}
