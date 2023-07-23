1f679d8da52e934ec257e6dcde40ac884d4d2fc6
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-07-23 15:00:20 +0900

    問題文とコメントを追加+変数名cntをcurに変更

--- ABC/311/b.cpp
+++ ABC/311/b.cpp
@@ -1,6 +1,19 @@
+/* 問題文
+1 からN までの番号がついたN 人の人がいます。
+N 人の人の今後D 日間の予定が与えられます。人i の予定は長さ
+D の文字列Si で表されて、Si のj 文字目が o ならばj 日目は暇であることを、x ならばそうでないことを意味します。
+D 日間のうち全員が暇であるような 連続する 何日かを選ぶことを考えます。
+選べる日数は最大で何日ですか？ただし、選べる日が存在しない場合は
+0 日と答えてください。
+
+制約
+1≤N≤100
+1≤D≤100
+N,D は整数
+Si は o と x からなる長さD の文字列 */
+
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 int main() {
@@ -10,20 +23,21 @@ int main() {
     vector<string> s(n);
     for (int i = 0; i < n; ++i) cin >> s[i];
 
-    int cnt = 0, ans = 0;
+    int cur = 0, ans = 0;
     for (int i = 0; i < d; ++i) {
+
+        // 各日を見て、全員がoならcurを1増やす
         bool ok = true;
         for (int j = 0; j < n; ++j) {
             if (s[j][i] == 'x') ok = false;
         }
 
-        if (ok) cnt++;
-        else {
-            ans = max(ans, cnt);
-            cnt = 0;
-        }
+        if (ok) cur++;
+        else cur = 0; // 1人でもxなら、連続する日数がリセットされるので、0に戻す
+
+        ans = max(ans, cur);
     }
-    ans = max(ans, cnt);
+
     cout << ans << endl;
 
     return 0;
