f89a610f335147b70e96c7a727c7a0530729d02c
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-07-28 14:13:44 +0900

    自力AC

---
+++ ABC/246/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x1, x2, x3, y1, y2, y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+    if (x1 == x2) cout << x3 << " ";
+    else if (x1 == x3) cout << x2 << " ";
+    else if (x2 == x3) cout << x1 << " ";
+
+    if (y1 == y2) cout << y3 << endl;
+    else if (y2 == y3) cout << y1 << endl;
+    else if (y3 == y1) cout << y2 << endl;
+
+    return 0;
+}
