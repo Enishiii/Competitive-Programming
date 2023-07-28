44a5cab343744c05436144da5e0475aab035c1e5
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-07-28 14:23:50 +0900

    問題文追加＆リファクタリング

--- ABC/246/a.cpp
+++ ABC/246/a.cpp
@@ -1,17 +1,37 @@
+/* 問題文
+xy 平面上に長方形があります。この長方形の各辺は x 軸または y 軸に平行であり、面積は
+0 ではありません。
+この長方形の 4 つの頂点のうち異なる 3 つの頂点の座標 (x1,y1),(x2,y2),(x3,y3) が与えられるので、残る 1 つの頂点の座標を求めてください。
+
+制約
+−100≤xi,yi≤100
+(x1,y1),(x2,y2),(x3,y3) のすべてを頂点に持つ長方形がただ一つ存在し、その各辺は x 軸または y 軸に平行であり、面積は 0 ではない。
+入力はすべて整数 */
+
 #include <iostream>
 using namespace std;
 
+struct Point {
+    int x;
+    int y;
+};
+
+Point findFourthPoint(Point p1, Point p2, Point p3) {
+    Point p4;
+
+    p4.x = (p1.x == p2.x) ? p3.x : ((p1.x == p3.x) ? p2.x : p1.x);
+    p4.y = (p1.y == p2.y) ? p3.y : ((p1.y == p3.y) ? p2.y : p1.y);
+
+    return p4;
+}
+
 int main() {
-    int x1, x2, x3, y1, y2, y3;
-    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    Point p1, p2, p3;
+    cin >> p1.x >> p1.y >> p2.x >> p2.y >> p3.x >> p3.y;
 
-    if (x1 == x2) cout << x3 << " ";
-    else if (x1 == x3) cout << x2 << " ";
-    else if (x2 == x3) cout << x1 << " ";
+    Point p4 = findFourthPoint(p1, p2, p3);
 
-    if (y1 == y2) cout << y3 << endl;
-    else if (y2 == y3) cout << y1 << endl;
-    else if (y3 == y1) cout << y2 << endl;
+    cout << p4.x << " " << p4.y << endl;
 
     return 0;
 }
