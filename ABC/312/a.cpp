c91d3a1d5796cd16a81522f43efa032f3a06800b
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-07-29 22:45:14 +0900

    自力AC

---
+++ ABC/312/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (s == "ACE" || s == "BDF" || s == "CEG" || s == "DFA" || s == "EGB" || s == "FAC" || s == "GBD") {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
