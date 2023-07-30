4e371b6359dfafe96fb9f2b2b6b20c242c2edff6
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-07-30 12:19:46 +0900

    公式解説を参考にリファクタリング

--- ABC/312/a.cpp
+++ ABC/312/a.cpp
@@ -1,17 +1,20 @@
+/* 問題文
+英大文字からなる長さ3 の文字列S が与えられます。
+S が ACE、BDF、CEG、DFA、EGB、FAC、GBD のいずれかと等しいとき Yes を、そうでないとき No を出力してください。
+
+制約
+S は英大文字からなる長さ3 の文字列 */
+
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <string>
 using namespace std;
 
 int main() {
     string s;
     cin >> s;
 
-    if (s == "ACE" || s == "BDF" || s == "CEG" || s == "DFA" || s == "EGB" || s == "FAC" || s == "GBD") {
-        cout << "Yes" << endl;
-    } else {
-        cout << "No" << endl;
-    }
+    string t = "ACEGBDFAC";
+    cout << (t.find(s) == string::npos ? "No" : "Yes") << endl;
 
     return 0;
 }
