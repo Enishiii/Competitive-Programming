3f615ac24d30c54ff532935b92c58e40f1d7269a
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-09-03 11:02:05 +0900

    問題文とコメントを追加

--- ABC/318/b.cpp
+++ ABC/318/b.cpp
@@ -1,3 +1,16 @@
+/* 問題文
+座標平面上に N 枚の長方形のシートが張られています。
+各シートが覆う長方形領域の各辺はそれぞれ x 軸または y 軸と平行であり、
+具体的には、i 枚目のシートはちょうど Ai≤x≤Bi かつCi≤y≤Di をみたす領域全体を覆っています。
+1 枚以上のシートによって覆われている領域 の面積を S とすると、 S は制約の条件下で整数となる事が証明できます。
+S を整数の形で出力してください。
+
+制約
+2 ≤ N ≤ 100
+0 ≤ Ai < Bi ≤ 100
+0 ≤ Ci < Di ≤ 100
+入力はすべて整数 */
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -11,19 +24,23 @@ int main() {
         cin >> a[i] >> b[i] >> c[i] >> d[i];
     }
 
-    int area = 0;
+    int coveredArea = 0;
+
+    // 0から100までのすべての座標点を順にチェック
     for (int x = 0; x <= 100; ++x) {
         for (int y = 0; y <= 100; ++y) {
+
+            // その点が1つ以上のシートで覆われているかを判定する
             for (int i = 0; i < n; ++i) {
                 if (a[i] <= x && x < b[i] && c[i] <= y && y < d[i]) {
-                    area++;
+                    coveredArea++;
                     break; // 1つ以上のシートで覆われているので、次の座標に移動する
                 }
             }
         }
     }
 
-    cout << area << endl;
+    cout << coveredArea << endl;
 
     return 0;
 }
