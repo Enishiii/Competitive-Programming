4cc4736402efcdb1e4b116d9a08bae9a632c320d
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-09-03 12:14:16 +0900

    累積和を使う解法に修正

--- ABC/318/c.cpp
+++ ABC/318/c.cpp
@@ -1,3 +1,18 @@
+/* 問題文
+高橋君は N 日間の鉄道旅行を計画しています。
+高橋君はそれぞれの日について、運賃の通常料金を払うか、1 日周遊パスを 1 枚使用するか選ぶことができます。
+ここで、1≤i≤N について、i 日目の旅行にかかる運賃の通常料金は Fi 円です。
+一方、1 日周遊パスはD 枚セットでP 円で発売されており、何セットでも購入することが可能ですが、D 枚単位でしか購入することができません。
+また、購入したパスは 1 枚ずつ好きな日に使うことができ、旅行が終了した時点で余っていても構いません。
+N 日間の旅行でかかる金額、すなわち 1 日周遊パスの購入にかかった代金と、1 日周遊パスを利用しなかった日における運賃の通常料金の合計金額の和としてあり得る最小値を求めてください。
+
+制約
+1 ≤ N ≤ 2×10^5
+1 ≤ D ≤ 2×10^5
+1 ≤ P ≤ 10^9
+1 ≤ Fi ≤ 10^9
+入力はすべて整数 */
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -5,32 +20,34 @@ using namespace std;
 using ll = long long;
 
 int main() {
-    ll n, d, p;
+    int n, d, p;
     cin >> n >> d >> p;
-    vector<ll> f(n);
-    for (int i = 0; i < n; ++i) cin >> f[i];
+    vector<int> fares(n);
+    for (int i = 0; i < n; ++i) cin >> fares[i];
 
-    vector<ll> copy = f;
-    sort(copy.rbegin(), copy.rend());
+    sort(fares.begin(), fares.end());
 
-    ll ans = 0;
+    vector<ll> cumulativeSum(n+1);
     for (int i = 0; i < n; ++i) {
-        ll maxCostWithinD = 0;
-        for (int j = i; j < i + d && j < n; ++j) {
-            maxCostWithinD += copy[j];
-        }
-
-        if (maxCostWithinD > p) {
-            ans += p;
-            i += d - 1;
-        } else {
-            for (int j = i; j < i + d && j < n; ++j) {
-                ans += copy[j];
-            }
-            i += d - 1;
-        }
+        cumulativeSum[i+1] = cumulativeSum[i] + fares[i];
     }
-    cout << ans << endl;
+
+    ll minCost = cumulativeSum[n];
+    for (int i = 0; i < 1e9; ++i) { // i: パスを購入するセット数
+        int daysToRegularFare = max(0, n - i * d);
+
+        // パスのセット数*d日分の値段を除去した、通常価格の合計
+        ll costWithoutPass = cumulativeSum[daysToRegularFare];
+
+        // 通常価格の合計に、パスの値段p*セット数iを足して合計金額を出す
+        ll totalCost = costWithoutPass + (ll)p * i;
+
+        minCost = min(minCost, totalCost);
+
+        if (daysToRegularFare == 0) break;
+    }
+
+    cout << minCost << endl;
 
     return 0;
 }
