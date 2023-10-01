a99b14046fb24ea5c4bedd104a7d6d085ebeaf3b
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-10-01 10:49:54 +0900

    自力AC

---
+++ ABC/322/b.cpp
@@ -0,0 +1,46 @@
+/*
+問題文
+英小文字からなる文字列S,T が与えられます。S の長さはN、T の長さは
+M です。(N≤M が制約で保証されています)
+S がT の 接頭辞 であるとは、T のはじめN 文字からなる文字列が
+S と一致することを言います。
+S がT の 接尾辞 であるとは、T の後ろN 文字からなる文字列が
+S と一致することを言います。
+S がT の接頭辞であり、かつ接尾辞でもある場合は0 を、
+S がT の接頭辞であるが、接尾辞でない場合は1 を、
+S がT の接尾辞であるが、接頭辞でない場合は2 を、
+S がT の接頭辞でも接尾辞でもない場合は3 を出力してください。
+
+制約
+1≤N≤M≤100
+S は英小文字からなる長さ
+N の文字列
+T は英小文字からなる長さ
+M の文字列
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    string s, t;
+    cin >> s >> t;
+
+    bool prefix = true;
+    for (int i = 0; i < n; ++i) {
+        if (s[i] != t[i]) prefix = false;
+    }
+
+    bool suffix = true;
+    if (t.substr(m-n, n) != s) suffix = false;
+
+    if (prefix && suffix) cout << 0 << endl;
+    else if (prefix) cout << 1 << endl;
+    else if (suffix) cout << 2 << endl;
+    else cout << 3 << endl;
+
+    return 0;
+}
