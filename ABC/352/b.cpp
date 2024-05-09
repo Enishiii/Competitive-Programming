3265fd50438a003488082be99b3baa9f49837ddf
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-05-09 21:41:57 +0900

    リファクタリング：判定と出力を別の関数に切り分け

--- ABC/352/b.cpp
+++ ABC/352/b.cpp
@@ -1,28 +1,46 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-string readInputString() {
-    string input;
-    cin >> input;
-    return input;
+string readInputString()
+{
+    string inputString;
+    cin >> inputString;
+    return inputString;
 }
 
-void printMatchingPositions(const string& target, const string& source) {
-    int targetIndex = 0;
-    for (int i = 0; i < source.size(); i++) {
-        if (source[i] == target[targetIndex]) {
-            cout << i + 1 << " ";
-            targetIndex++;
+vector<int> findMatchingIndices(const string &pattern, const string &source)
+{
+    vector<int> matchingIndices;
+    int patternIndex = 0;
+    for (int i = 0; i < source.size(); ++i)
+    {
+        if (source[i] == pattern[patternIndex])
+        {
+            matchingIndices.push_back(i + 1);
+            patternIndex++;
         }
+    }
+    return matchingIndices;
+}
+
+void printMatchingIndices(const vector<int> &matchingIndices)
+{
+    for (int index : matchingIndices)
+    {
+        cout << index << " ";
     }
     cout << endl;
 }
 
-int main() {
-    string target = readInputString();
+int main()
+{
+    string pattern = readInputString();
     string source = readInputString();
 
-    printMatchingPositions(target, source);
+    vector<int> matchingIndices = findMatchingIndices(pattern, source);
+
+    printMatchingIndices(matchingIndices);
 
     return 0;
 }
