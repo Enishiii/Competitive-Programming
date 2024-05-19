f9279946266fe3ae884acdc729917ee4332027a5
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-05-19 23:07:47 +0900

    ビット演算の実装に変更

--- ABC/354/a.cpp
+++ ABC/354/a.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main() {
     long long h;
     cin >> h;
 
-    int plant = 0;
-    for (int i = 0; ; ++i) {
-        plant += pow(2, i);
-
-        if (h < plant) {
-            cout << i + 1 << endl;
-            return 0;
-        }
+    int ans = 0;
+    long long now = 0;
+    while(now <= h) {
+        now += 1LL << ans;
+        ans++;
     }
-
+    cout << ans << endl;
+    
     return 0;
 }
