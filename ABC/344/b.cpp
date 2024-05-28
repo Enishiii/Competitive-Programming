21073e19f04156accc170e7d9d709e0c50c5f1d7
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-05-28 21:08:02 +0900

    関数の切り分け

--- ABC/344/b.cpp
+++ ABC/344/b.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-int main() {
+vector<int> readArray() {
     vector<int> numbers;
 
     while(true) {
@@ -14,10 +14,21 @@ int main() {
         numbers.push_back(input);
     }
 
+    return numbers;
+}
+
+void printReversedArray(const vector<int>& numbers) {
     int size = numbers.size();
+
     for (int i = size-1; i >= 0; --i) {
         cout << numbers[i] << endl;
     }
+}
+
+int main() {
+    vector<int> numbers = readArray();
+
+    printReversedArray(numbers);
 
     return 0;
 }
