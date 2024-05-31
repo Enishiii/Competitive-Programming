5293f13038183a7e4b760012d81034e99a4a8712
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-05-31 22:46:49 +0900

    クラスを作成して、その中に処理を集中させる

--- ABC/339/b.cpp
+++ ABC/339/b.cpp
@@ -2,62 +2,94 @@
 #include <vector>
 using namespace std;
 
-const int UP = 0;
-const int RIGHT = 1;
-const int DOWN = 2;
-const int LEFT = 3;
+class Grid {
+public:
+    Grid(int height, int width): height_(height), width_(width), isBlack_(height, vector<bool>(width, false)), row_(0), col_(0), direction_(UP) {}
 
-int turnRight(int direction) {
-    return (direction + 1) % 4;
-}
+    void performOperations(int numOperations) {
+        for (int i = 0; i < numOperations; ++i) {
+            moveAndPaint();
+        }
+    }
 
-int turnLeft(int direction) {
-    return (direction + 3) % 4;
-}
+    void print() const {
+        for (int i = 0; i < height_; ++i) {
+            for (int j = 0; j < width_; ++j) {
+                cout << (isBlack_[i][j] ? BLACK : WHITE);
+            }
+            cout << endl;
+        }
+    }
+
+private:
+    int height_;
+    int width_;
+    vector<vector<bool>> isBlack_;
+    int row_;
+    int col_;
+    int direction_;
+
+    static const char WHITE = '.';
+    static const char BLACK = '#';
+
+    static const int UP = 0;
+    static const int RIGHT = 1;
+    static const int DOWN = 2;
+    static const int LEFT = 3;
+
+    int turnRight(int direction) {
+        return (direction + 1) % 4;
+    }
+
+    int turnLeft(int direction) {
+        return (direction + 3) % 4;
+    }
+
+    void moveForward() {
+        switch(direction_) {
+            case UP:
+                row_ = (row_ - 1 + height_) % height_;
+                break;
+            case RIGHT:
+                col_ = (col_ + 1) % width_;
+                break;
+            case DOWN:
+                row_ = (row_ + 1) % height_;
+                break;
+            case LEFT:
+                col_ = (col_ - 1 + width_) % width_;
+                break;
+        }
+    }
+
+    void paintSquare() {
+        isBlack_[row_][col_] = !isBlack_[row_][col_];
+    }
+
+    void moveAndPaint() {
+        bool isWhite = !isBlack_[row_][col_];
+
+        paintSquare();
+
+        if (isWhite) {
+            direction_ = turnRight(direction_);
+        } else {
+            direction_ = turnLeft(direction_);
+        }
+
+        moveForward();
+    }
+};
 
 int main() {
     int height, width, numOperations;
     cin >> height >> width >> numOperations;
 
-    vector<vector<bool>> grid(height, vector<bool>(width, false));
+    Grid grid(height, width);
 
-    int currentRow = 0;
-    int currentCol = 0;
-    int direction = UP;
+    grid.performOperations(numOperations);
 
-    for (int i = 0; i < numOperations; ++i) {
-        bool isWhite = !grid[currentRow][currentCol];
-
-        grid[currentRow][currentCol] = !grid[currentRow][currentCol];
-
-        if (isWhite) {
-            direction = turnRight(direction);
-        } else {
-            direction = turnLeft(direction);
-        }
-
-        switch(direction) {
-            case UP:
-                currentRow = (currentRow - 1 + height) % height;
-                break;
-            case RIGHT:
-                currentCol = (currentCol + 1) % width;
-                break;
-            case DOWN:
-                currentRow = (currentRow + 1) % height;
-                break;
-            case LEFT:
-                currentCol = (currentCol - 1 + width) % width;
-                break;
-        }
-    }
-
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
-            cout << (grid[i][j] ? '#' : '.');
-        }
-        cout << endl;
-    }
+    grid.print();
 
     return 0;
 }
