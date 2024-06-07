f5b84c5d026672ac4f943fc5c4f380807c4a5ec2
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-06-07 21:58:44 +0900

    シンプルなビット演算と、整数への操作に置き換える解法を実装

--- ABC/336/b.cpp
+++ ABC/336/b.cpp
@@ -1,33 +1,25 @@
 #include <iostream>
-#include <bitset>
-#include <string>
 using namespace std;
-
-string convertBinaryFrom(int number) {
-    bitset<50> bs(number);
-
-    string binary = bs.to_string();
-    return binary;
-}
-
-int ctz(string binary) {
-    int countZero = 0;
-    for (int i = binary.size()-1; i >= 0; --i) {
-        if (binary[i] == '1') break;
-
-        countZero++;
-    }
-    return countZero;
-}
 
 int main() {
     int number;
     cin >> number;
 
-    string binary = convertBinaryFrom(number);
+    // for (int i = 0; ; ++i) {
+    //     if (number & (1 << i)) {
+    //         cout << i << endl;
+    //         return 0;
+    //     }
+    // }
 
-    int result = ctz(binary);
-    cout << result << endl;
+    int count = 0;
+    while(number > 0) {
+        if (number % 2 == 1) break;
+
+        count++;
+        number /= 2;
+    }
+    cout << count << endl;
 
     return 0;
 }
