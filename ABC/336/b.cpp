6ce5d6beff1aa0729fcb6dd1f041dd5836f97acc
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-06-07 21:59:24 +0900

    シンプルなビット演算を選択

--- ABC/336/b.cpp
+++ ABC/336/b.cpp
@@ -5,21 +5,21 @@ int main() {
     int number;
     cin >> number;
 
-    // for (int i = 0; ; ++i) {
-    //     if (number & (1 << i)) {
-    //         cout << i << endl;
-    //         return 0;
-    //     }
-    // }
-
-    int count = 0;
-    while(number > 0) {
-        if (number % 2 == 1) break;
-
-        count++;
-        number /= 2;
+    for (int i = 0; ; ++i) {
+        if (number & (1 << i)) {
+            cout << i << endl;
+            return 0;
+        }
     }
-    cout << count << endl;
+
+    // int count = 0;
+    // while(number > 0) {
+    //     if (number % 2 == 1) break;
+
+    //     count++;
+    //     number /= 2;
+    // }
+    // cout << count << endl;
 
     return 0;
 }
