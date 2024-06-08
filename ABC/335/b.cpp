5e4e28d387e66b71e63e1f09826bb022f19d2126
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-06-08 21:17:57 +0900

    関数に切り出す

--- ABC/335/b.cpp
+++ ABC/335/b.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 using namespace std;
 
-
-int main() {
-    int n;
-    cin >> n;
-
-    for (int x = 0; x <= n; x++) {
-        for (int y = 0; y <= n; y++) {
-            for (int z = 0; z <= n; z++) {
+void printCombinations(int n) {
+    for (int x = 0; x <= n; ++x) {
+        for (int y = 0; y <= n; ++y) {
+            for (int z = 0; z <= n; ++z) {
                 if ((x + y + z) > n) continue;
 
                 cout << x << " " << y << " " << z << endl;
             }
         }
     }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    printCombinations(n);
 
     return 0;
 }
