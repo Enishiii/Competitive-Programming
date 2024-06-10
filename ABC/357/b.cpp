3618ef40b582007a9781763e279800c3f7a4173a
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-06-10 22:25:45 +0900

    自力AC

---
+++ ABC/357/b.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    string s;
+    cin >> s;
+
+    int lower = 0, upper = 0;
+    for (auto& c : s) {
+        (islower(c) ? lower : upper)++;
+    }
+
+    if (lower < upper) {
+        for (auto& c : s) {
+            if (islower(c)) c = toupper(c);
+        }
+    } else {
+        for (auto& c : s) {
+            if (isupper(c)) c = tolower(c);
+        }
+    }
+    cout << s << "\n";
+
+    return 0;
+}
