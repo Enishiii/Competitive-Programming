6e9a4a8f46197a33f41b831b9db1e2b1e274928a
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-06-14 22:50:05 +0900

    関数に切り分け

--- ABC/328/b.cpp
+++ ABC/328/b.cpp
@@ -7,6 +7,20 @@ bool isZorome(const string& date) {
     return size(set<char>(date.begin(), date.end())) == 1;
 }
 
+int countZoromeDate(const vector<int>& days) {
+    int n = days.size();
+    int zoromeCount = 0;
+    for (int m = 1; m <= n; ++m) {
+        string month = to_string(m);
+        for (int d = 1; d <= days[m - 1]; ++d) {
+            string date = month + to_string(d);
+            if (!isZorome(date)) continue;
+            zoromeCount++;
+        }
+    }
+    return zoromeCount;
+}
+
 int main() {
     int n;
     cin >> n;
@@ -14,15 +28,7 @@ int main() {
     vector<int> days(n);
     for (auto& day : days) cin >> day;
 
-    int zoromeCount = 0;
-    for (int m = 1; m <= n; ++m) {
-        string month = to_string(m);
-
-        for (int d = 1; d <= days[m - 1]; ++d) {
-            string date = month + to_string(d);
-            if (isZorome(date)) zoromeCount++;
-        }
-    }
+    int zoromeCount = countZoromeDate(days);
     cout << zoromeCount << endl;
 
     return 0;
