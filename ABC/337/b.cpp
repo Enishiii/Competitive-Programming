03b9ec0cc0ab2c8ed83b9f782d6a628afacb9667
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-06-18 21:10:28 +0900

    他の解法を追加した

--- ABC/337/b.cpp
+++ ABC/337/b.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <regex>
+#include <algorithm>
 using namespace std;
 
 int main() {
     string s;
     cin >> s;
 
-    if (regex_match(s, regex("A*B*C*"))) {
+    // if (regex_match(s, regex("A*B*C*"))) {
+    //     cout << "Yes" << endl;
+    // } else {
+    //     cout << "No" << endl;
+    // }
+
+    if (ranges::is_sorted(s)) {
         cout << "Yes" << endl;
     } else {
         cout << "No" << endl;
