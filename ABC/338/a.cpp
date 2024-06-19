6c694c3ba311ef4f843b36f8873e634f7191ef0d
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-06-19 19:51:20 +0900

    all_ofを使用する実装に変更し、関数も統合した

--- ABC/338/a.cpp
+++ ABC/338/a.cpp
@@ -1,28 +1,20 @@
 #include <iostream>
 #include <cctype>
 #include <string>
+#include <algorithm>
 using namespace std;
 
-bool startsWithUpper(const string& s) {
-    return !s.empty() && isupper(s[0]);
-}
+bool isValidString(const string& s) {
+    if (s.empty() || !isupper(s[0])) return false;
 
-bool restAreLower(const string& s) {
-    for (int i = 1; i < s.size(); ++i) {
-        if (!islower(s[i])) return false;
-    }
-    return true;
+    return all_of(s.begin() + 1, s.end(), ::islower);
 }
 
 int main() {
     string s;
     cin >> s;
 
-    if (startsWithUpper(s) && restAreLower(s)) {
-        cout << "Yes" << endl;
-    } else {
-        cout << "No" << endl;
-    }
+    cout << (isValidString(s) ? "Yes" : "No") << endl;
 
     return 0;
 }
