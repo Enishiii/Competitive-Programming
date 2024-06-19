4fef4d3f55fbbeaed0f9cd595f6a46d693b6873d
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-06-19 19:45:36 +0900

    関数の分離と標準ライブラリのインクルードを実装した

--- ABC/338/a.cpp
+++ ABC/338/a.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
+#include <cctype>
+#include <string>
 using namespace std;
 
+bool startsWithUpper(const string& s) {
+    return !s.empty() && isupper(s[0]);
+}
+
+bool restAreLower(const string& s) {
+    for (int i = 1; i < s.size(); ++i) {
+        if (!islower(s[i])) return false;
+    }
+    return true;
+}
 
 int main() {
     string s;
     cin >> s;
 
-    if (!isupper(s[0])) {
+    if (startsWithUpper(s) && restAreLower(s)) {
+        cout << "Yes" << endl;
+    } else {
         cout << "No" << endl;
-        return 0;
     }
-
-    for (int i = 1; i < s.size(); ++i) {
-        if (!islower(s[i])) {
-            cout << "No" << endl;
-            return 0;
-        }
-    }
-    cout << "Yes" << endl;
 
     return 0;
 }
