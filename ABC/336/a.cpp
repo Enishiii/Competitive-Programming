888723cad8dad7ee44ab7262c5d881950133913f
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-06-21 20:49:44 +0900

    自力AC

---
+++ ABC/336/a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string answer = "L" + string(n, 'o') + "ng";
+    cout << answer << endl;
+
+    return 0;
+}
