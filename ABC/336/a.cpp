20b9a014a56994cbd729338d5f1a4eeb66e4e2be
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-06-21 20:52:52 +0900

    定数を定義した

--- ABC/336/a.cpp
+++ ABC/336/a.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 
+const string PREFIX = "L";
+const string SUFFIX = "ng";
+
 int main() {
     int n;
     cin >> n;
 
-    string answer = "L" + string(n, 'o') + "ng";
+    string answer = PREFIX + string(n, 'o') + SUFFIX;
     cout << answer << endl;
 
     return 0;
