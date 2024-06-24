450c9f8bceafcf276ee69f141caeeb111be40d2d
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-06-24 20:11:53 +0900

    関数に分割した

--- ABC/359/b.cpp
+++ ABC/359/b.cpp
@@ -2,19 +2,30 @@
 #include <vector>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
+vector<int> readInput(int size) {
+    vector<int> array(size);
+    for (int i = 0; i < size; ++i) cin >> array[i];
+    return array;
+}
 
-    vector<int> a(2 * n);
-    for (int i = 0; i < 2 * n; ++i) {
-        cin >> a[i];
-    }
-
+int countEqualPairs(const vector<int>& colors, int size) {
     int count = 0;
-    for (int i = 0; i < 2 * n - 2; ++i) {
-        if (a[i] == a[i + 2]) count++;
+    for (int i = 0; i < size - 2; ++i) {
+        if (colors[i] != colors[i + 2]) continue;
+
+        count++;
     }
+    return count;
+}
+
+int main() {
+    int numberOfPairs;
+    cin >> numberOfPairs;
+
+    int size = numberOfPairs * 2;
+    vector<int> colors = readInput(size);
+
+    int count = countEqualPairs(colors, size);
     cout << count << endl;
 
     return 0;
