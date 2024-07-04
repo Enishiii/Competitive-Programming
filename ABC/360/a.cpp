4d57eae69e79135a1518162847a68bf2f5a6e333
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-07-04 20:00:44 +0900

    条件式を1行にまとめた

--- ABC/360/a.cpp
+++ ABC/360/a.cpp
@@ -5,9 +5,7 @@ int main() {
     string s;
     cin >> s;
 
-    if (s[0] == 'R') {
-        cout << "Yes" << endl;
-    } else if (s[1] == 'R' && s[2] == 'M') {
+    if (s[0] == 'R' || (s[1] == 'R' && s[2] == 'M')) {
         cout << "Yes" << endl;
     } else {
         cout << "No" << endl;
