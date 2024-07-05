119b606b1a129ce67bab9443443bbec5f736ec41
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-07-05 22:39:25 +0900

    変数名を改善し、関数に分割した

--- ABC/360/b.cpp
+++ ABC/360/b.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string s, t;
-    cin >> s >> t;
-
-    for (int w = 1; w < s.size(); ++w) {
-        for (int c = 0; c < w; ++c) {
-            string current = "";
-            for (int j = c; j < s.size(); j += w) {
-                current += s[j];
+bool checkPatternMatch(const string& source, const string& target) {
+    for (int windowSize = 1; windowSize < source.size(); ++windowSize) {
+        for (int startIndex = 0; startIndex < windowSize; ++startIndex) {
+            string constructedString = "";
+            for (int currentIndex = startIndex; currentIndex < source.size(); currentIndex += windowSize) {
+                constructedString += source[currentIndex];
             }
 
-            if (current == t) {
-                cout << "Yes" << endl;
-                return 0;
+            if (constructedString == target) {
+                return true;
             }
         }
     }
-    cout << "No" << endl;
+    return false;
+}
+
+int main() {
+    string source, target;
+    cin >> source >> target;
+
+    if (checkPatternMatch(source, target)) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
 
     return 0;
 }
