8f36e15ac54f7859127ffc0c63b802bdf7ccc3c1
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-07-06 16:23:27 +0900

    可読性を高めるためにクラスを導入

--- ABC/360/b.cpp
+++ ABC/360/b.cpp
@@ -1,29 +1,42 @@
 #include <iostream>
 using namespace std;
 
-bool checkPatternMatch(const string& source, const string& target) {
-    for (int windowSize = 1; windowSize < source.size(); ++windowSize) {
-        for (int startIndex = 0; startIndex < windowSize; ++startIndex) {
-            string constructed = "";
-            for (int currentIndex = startIndex; currentIndex < source.size(); currentIndex += windowSize) {
-                constructed += source[currentIndex];
+class StringPatternChecker {
+public:
+    StringPatternChecker(const string& source, const string& target) : source_(source), target_(target) {}
 
-                if (constructed.size() > target.size()) break;
-            }
+    bool matchesPattern() {
+        for (int windowSize = 1; windowSize < source_.size(); ++windowSize) {
+            for (int startIndex = 0; startIndex < windowSize; ++startIndex) {
+                string constructed = constructPattern(windowSize, startIndex);
 
-            if (constructed == target) {
-                return true;
+                if (constructed == target_) {
+                    return true;
+                }
             }
         }
+        return false;
     }
-    return false;
-}
+
+private:
+    string source_;
+    string target_;
+
+    string constructPattern(int windowSize, int startIndex) {
+        string result;
+        for (int i = startIndex; i < source_.size(); i += windowSize) {
+            result += source_[i];
+        }
+        return result;
+    }
+};
 
 int main() {
     string source, target;
     cin >> source >> target;
 
-    if (checkPatternMatch(source, target)) {
+    StringPatternChecker checker(source, target);
+    if (checker.matchesPattern()) {
         cout << "Yes" << endl;
     } else {
         cout << "No" << endl;
