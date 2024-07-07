c74f5e4ff86e519c35b990cf810bc40cb670f77a
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-07-07 16:21:38 +0900

    クラスを導入

--- ABC/332/c.cpp
+++ ABC/332/c.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int numDays, initialPlainShirts;
-    cin >> numDays >> initialPlainShirts;
-
+class TShirtCalculator {
+private:
+    int numDays;
+    int initialPlainShirts;
     string schedule;
-    cin >> schedule;
-    schedule += '0';
 
-    int maxShirtsNeeded = 0;
-    int plainCount = 0, logoCount = 0;
+    int minimumShirtsNeeded = 0;
+    int plainCount = 0;
+    int logoCount = 0;
 
-    for (int i = 0; i <= numDays; ++i) {
-        if (schedule[i] == '0') {
-            maxShirtsNeeded = max(maxShirtsNeeded, max(plainCount + logoCount - initialPlainShirts, logoCount));
-            plainCount = 0, logoCount = 0;
-        }
-        if (schedule[i] == '1') plainCount++;
-        if (schedule[i] == '2') logoCount++;
+public:
+    void readInput() {
+        cin >> numDays >> initialPlainShirts;
+        cin >> schedule;
+        schedule += '0';
     }
-    cout << maxShirtsNeeded << endl;
+
+    int calculateMinimumShirtsNeeded() {
+        for (int i = 0; i <= numDays; ++i) {
+            if (schedule[i] == '0') {
+                minimumShirtsNeeded = max(minimumShirtsNeeded, max(plainCount + logoCount - initialPlainShirts, logoCount));
+                plainCount = 0, logoCount = 0;
+            } else if (schedule[i] == '1') {
+                plainCount++;
+            } else if (schedule[i] == '2') {
+                logoCount++;
+            }
+        }
+        return minimumShirtsNeeded;
+    }
+};
+
+int main() {
+    TShirtCalculator calculator;
+    calculator.readInput();
+
+    int result = calculator.calculateMinimumShirtsNeeded();
+    cout << result << endl;
 
     return 0;
 }
