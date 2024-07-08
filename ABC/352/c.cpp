7c7675891c579e326c7a6f52376535d25a135d32
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-07-08 21:35:04 +0900

    関数に分割し、引数を減らすために構造体を導入した

--- ABC/352/c.cpp
+++ ABC/352/c.cpp
@@ -2,19 +2,47 @@
 #include <vector>
 using namespace std;
 
+struct Giant {
+    long long shoulderHeight;
+    long long headHeight;
+};
+
+void readInput(int numGiants, vector<Giant>& giants) {
+    cin >> numGiants;
+    giants.resize(numGiants);
+    for (int i = 0; i < numGiants; ++i) {
+        cin >> giants[i].shoulderHeight >> giants[i].headHeight;
+    }
+}
+
+long long calculateMaxHeadShoulderDifference(const vector<Giant>& giants) {
+    long long maxHeadShoulderDifference = 0;
+    for (const auto& giant : giants) {
+        maxHeadShoulderDifference = max(maxHeadShoulderDifference, giant.headHeight - giant.shoulderHeight);
+    }
+    return maxHeadShoulderDifference;
+}
+
+long long sumUpShoulderHeights(const vector<Giant>& giants) {
+    long long sum = 0;
+    for (const auto& giant : giants) {
+        sum += giant.shoulderHeight;
+    }
+    return sum;
+}
+
 int main() {
     int numGiants;
-    cin >> numGiants;
+    vector<Giant> giants;
 
-    vector<long long> shoulders(numGiants), heads(numGiants);
-    for (int i = 0; i < numGiants; ++i) cin >> shoulders[i] >> heads[i];
+    readInput(numGiants, giants);
 
-    long long maxDiffBetweenShoulderAndHead = 0;
-    for (int i = 0; i < numGiants; ++i) maxDiffBetweenShoulderAndHead = max(maxDiffBetweenShoulderAndHead, heads[i] - shoulders[i]);
+    long long maxHeadShoulderDifference = calculateMaxHeadShoulderDifference(giants);
 
-    long long ans = maxDiffBetweenShoulderAndHead;
-    for (int i = 0; i < numGiants; ++i) ans += shoulders[i];
-    cout << ans << endl;
+    long long sumOfShoulderHeights = sumUpShoulderHeights(giants);
+
+    long long result = maxHeadShoulderDifference + sumOfShoulderHeights;
+    cout << result << endl;
 
     return 0;
 }
