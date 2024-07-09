29ea9103834ba72d3c844b81e7926c95d09c9088
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-07-09 21:40:55 +0900

    愚直かつシンプルな解法にした

--- ABC/213/a.cpp
+++ ABC/213/a.cpp
@@ -5,7 +5,12 @@ int main() {
     int a, b;
     cin >> a >> b;
 
-    cout << (a ^ b) << endl;
+    for (int c = 0; c <= 255; ++c) {
+        if ((a^c) == b) {
+            cout << c << endl;
+            return 0;
+        }
+    }
 
     return 0;
 }
