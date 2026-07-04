da71e13a9bcacfb22cd2fbd84deb8bdc2f60e105
Enishi Yoshioka <enishiyoshioka@gmail.com>
2026-07-04 12:37:32 +0900

    palindrome-number を文字列変換なしで解いた

--- leet-code/9.palindrome-number/palindrome_number.go
+++ leet-code/9.palindrome-number/palindrome_number.go
@@ -1,21 +1,36 @@
 package palindromenumber
 
-import "strconv"
+// import "strconv"
 
 func isPalindrome(x int) bool {
-	s := strconv.Itoa(x)
-
-	left := 0
-	right := len(s) - 1
-
-	for left < right {
-		if s[left] != s[right] {
-			return false
-		}
-
-		left++
-		right--
+	if x < 0 {
+		return false
 	}
 
-	return true
+	if x != 0 && x%10 == 0 {
+		return false
+	}
+
+	reversed := 0
+	for x > reversed {
+		reversed = reversed*10 + x%10
+		x /= 10
+	}
+
+	return x == reversed || x == reversed/10
+	// s := strconv.Itoa(x)
+
+	// left := 0
+	// right := len(s) - 1
+
+	// for left < right {
+	// 	if s[left] != s[right] {
+	// 		return false
+	// 	}
+
+	// 	left++
+	// 	right--
+	// }
+
+	// return true
 }
