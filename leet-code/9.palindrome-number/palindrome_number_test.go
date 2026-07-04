9452144f2434af49b6466d09d4d98c4f80827410
Enishi Yoshioka <enishiyoshioka@gmail.com>
2026-07-04 12:26:32 +0900

    go fmt を実行

--- leet-code/9.palindrome-number/palindrome_number_test.go
+++ leet-code/9.palindrome-number/palindrome_number_test.go
@@ -5,48 +5,48 @@ import "testing"
 func TestIsPalindrome(t *testing.T) {
 	tests := []struct {
 		name string
-		x int
+		x    int
 		want bool
 	}{
 		{
 			name: "positive palindrome",
-			x: 121,
+			x:    121,
 			want: true,
 		},
 		{
 			name: "negative number",
-			x: -121,
+			x:    -121,
 			want: false,
 		},
 		{
 			name: "trailing zero",
-			x: 10,
+			x:    10,
 			want: false,
 		},
 		{
 			name: "zero",
-			x: 0,
+			x:    0,
 			want: true,
 		},
 		{
 			name: "even digits palindrome",
-			x: 1221,
+			x:    1221,
 			want: true,
 		},
 		{
 			name: "single digit",
-			x: 7,
+			x:    7,
 			want: true,
 		},
 		{
 			name: "not palindrome",
-			x: 123,
+			x:    123,
 			want: false,
 		},
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T)  {
+		t.Run(tt.name, func(t *testing.T) {
 			got := isPalindrome(tt.x)
 
 			if got != tt.want {
